4e14a9a117166789f1ce5e973f968e0d5f5cbb92
ryanmcgrath-utsw <90428872+ryanmcgrath-utsw@users.noreply.github.com>
2025-06-11 10:30:22 -0500

    add scatter2pnTable

--- scatter2pnTable.m
+++ scatter2pnTable.m
@@ -1,4 +1,4 @@
-function scatter2pnTable(ax, sigVal, lacksDisplayNames)
+function pnTable = scatter2pnTable(ax, sigVal, lacksDisplayNames)
 %SCATTER2PNTABLE takes an axis containing scatter plots and generates a table of p-Vals 
 % set lacks display names to true if you didn't add display names to
 % classify the groups (NOT RECOMMENDED)
@@ -22,7 +22,11 @@ for ii = 1:(length(cats)-1)
     for jj = ii+1:length(cats)
         dataSet1 = plottedData(ii).YData;
         dataSet2 = plottedData(jj).YData;
-        p = ranksum(dataSet1, dataSet2);
+        if isempty(dataSet1) || isempty(dataSet2)
+            p = 1;
+        else
+            p = ranksum(dataSet1, dataSet2);
+        end
         pnTable.Group1(end+1) = string(cats{ii});
         pnTable.Group2(end) = string(cats{jj});
         pnTable.n1(end) = length(dataSet1);
