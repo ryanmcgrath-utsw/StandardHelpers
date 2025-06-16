33310fcb3fbe7df7ad33100088f521bf3fb262b6
ryanmcgrath-utsw <90428872+ryanmcgrath-utsw@users.noreply.github.com>
2025-06-16 09:57:02 -0500

    improve createPPTfromFigures

--- createPPTfromFigures.m
+++ createPPTfromFigures.m
@@ -1,14 +1,27 @@
+function createPPTfromFigures(saveloc)
+
+arguments
+    saveloc (1,1) string = "N/A"
+end
+
 import mlreportgen.ppt.*
 
-[name,path] = uiputfile("*.pptx","Save Figures");
-if ~path, return, end
-saveloc = fullfile(path,name);
+if strcmp(saveloc, "N/A")
+    [name,path] = uiputfile("*.pptx","Save Figures");
+    if ~path, return, end
+    saveloc = fullfile(path,name);
+end
+
+if ~endsWith(saveloc,".pptx")
+    saveloc = saveloc+".pptx";
+end
 
 lb = StandardHelpers.loadBar(0,"Working...");
 
 resourceFolder = "/Users/ryan/Documents/MATLAB/+StandardHelpers/Resources";
 templateLocation = fullfile(resourceFolder,"ReportTemplate.pptx");
 ppt = Presentation(saveloc,templateLocation);
+if ~exist(fileparts(saveloc),"dir"), mkdir(fileparts(saveloc)), end
 open(ppt);
 
 allFigures = findobj("Type","Figure");
@@ -31,3 +44,4 @@ lb.progress = 1;
 lb.message = "Saving...";
 close(ppt);
 clear lb
+end
